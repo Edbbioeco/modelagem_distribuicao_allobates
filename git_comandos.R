ca4d707f86c80e945b149c97d1fafcfd1ffd49b7
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-14 10:03:43 -0300

    Scripts para comandos de Git

--- git_comandos.R
+++ git_comandos.R
@@ -2,13 +2,19 @@
 
 library(gert)
 
+# Listando arquivos ----
+
+gert::git_status() |>
+  as.data.frame()
+
 # Selecionando o arquivo ----
 
-gert::git_add(list.files(pattern = ".Rmd")) |> as.data.frame()
+gert::git_add(list.files(pattern = "README.md")) |>
+  as.data.frame()
 
 # Commitando ----
 
-gert::git_commit("RMarkdowns")
+gert::git_commit("README")
 
 # Pushando ----
 
@@ -17,3 +23,9 @@ gert::git_push(remote = "origin", force = TRUE)
 # Pullando ----
 
 gert::git_pull()
+
+# Resetando ----
+
+gert::git_reset_soft(ref = "HEAD~1")
+
+gert::git_reset_hard(ref = "HEAD~1")
